d0d37efa5837c29daec03ecd610d7b528d9dae94
tamnvm <tamnvm@vng.com.vn>
2022-11-03 01:00:38 +0700

    build

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={window.location.pathname.replace(/(\/[^/]+)$/, "")}>
       <App />
     </Router>
   </React.StrictMode>
